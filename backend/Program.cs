6e28d9b2e1a39c6eb626076535046b905947e996
Efekan Yilmaz <efese0707@gmail.com>
2026-09-12 20:52:19 +0300

    Add PostgreSQL EF Core foundation

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,14 @@
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
 
 var app = builder.Build();
 
